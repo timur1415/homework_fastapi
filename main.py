bca9d7bb4d2996d1a33f125e9e7c96524ebd3c5b
Тимур <timurmakssudov@gmail.com>
2025-12-12 22:44:44 +0300

    добавил валидацию, и добавил работу с фио

--- main.py
+++ main.py
@@ -1,32 +1,51 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel, field_validator 
+from pydantic import BaseModel, field_validator, ValidationError
+from typing import Union
 
 templates = Jinja2Templates(directory="templates")
 
+
 class Calc(BaseModel):
-    a : int
-    b: int
+    a: Union[int, float]
+    b: Union[int, float]
     sign: str
+
+    @field_validator("sign")
+    def validator_operation(cls, v):
+        if v not in ["+", "-", "*", "/"]:
+            raise ValueError("Недопустимая операция")
+        return v
+
+
+class Name(BaseModel):
+    first_name: str
+    last_name: str
+    surname: str
+
+
+class CalcOutput(BaseModel):
+    a: Union[int, float] = 0
+    b: Union[int, float] = 0
+    c: Union[int, float] = 0
+    sign: str = None
+    error: Union[str, None] = None
+
 
 app = FastAPI()
 
-def calculation(a: int, b:int, sign:str):
-    if sign == '-':
+
+def calculation(a: int, b: int, sign: str):
+    if sign == "-":
         return a - b
-    elif sign == '+':
+    elif sign == "+":
         return a + b
-    elif sign == '/':
+    elif sign == "/":
         return a / b
-    elif sign == '*':
+    elif sign == "*":
         return a * b
 
-    @field_validator('sign')
-    def validator_operation(cls, v):
-        if v not in ['+', '-', '*', '/']:
-            raise ValueError('Недопустимая операция')
-        return v
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
@@ -35,14 +54,37 @@ async def index(request: Request):
 
 @app.get("/calc", response_class=HTMLResponse)
 async def plus(request: Request, a: str, b: str, sign: str):
-    calc_input = Calc(a=a, b=b, sign=sign)
-    c = (calculation(calc_input.a, calc_input.b, calc_input.sign))
-    return templates.TemplateResponse(
-        "calc.html", {"request": request, "c": c}
-    )
+    try:
+        calc_input = Calc(a=a, b=b, sign=sign)
+        c = calculation(calc_input.a, calc_input.b, calc_input.sign)
+        calc_output = CalcOutput(a=a, b=b, sign=sign, c=c)
+        calc_output = CalcOutput(
+            a=calc_output.a, b=calc_output.b, c=calc_output.c, sign=calc_output.sign
+        )
+        return templates.TemplateResponse(
+            "calc.html", {"request": request, "calc_output": calc_output}
+        )
+    except ValidationError:
+        calc_output = CalcOutput(error="брат ты забыл что такое числа?")
+        return templates.TemplateResponse(
+            "calc.html",
+            {"request": request, "calc_output": calc_output},
+        )
+    except Exception:
+        calc_output = CalcOutput(error="что то непонятное ты делаешь")
+        return templates.TemplateResponse(
+            "calc.html", {"request": request, "calc_output": calc_output}
+        )
 
-# @app.get('/name', response_class=HTMLResponse)
-# async def name(request: Request, first_name: str, last_name: str, surname: str):
-#     return templates.TemplateResponse(
-#         'name.html', {"request": request, "first_name": first_name, 'last_name': last_name, "surname": surname}, 
-#     )
+
+@app.get("/name", response_class=HTMLResponse)
+async def name(request: Request, first_name: str, last_name: str, surname: str):
+    return templates.TemplateResponse(
+        "name.html",
+        {
+            "request": request,
+            "first_name": first_name,
+            "last_name": last_name,
+            "surname": surname,
+        },
+    )
